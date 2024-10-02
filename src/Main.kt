1fe982292757e50427f60c7f04deaaadf53854f5
Admin <reteamboss@gmail.com>
2024-10-02 16:33:30 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
 fun main() {
-    println("Hello World!")
+
+    val path = "C:/Users/Админ/Desktop/"
+    val name = "myfile2.txt"
+    val num = 30
+
+    val file = File(path + name)
+    val writer = FileWriter(file)
+    for (i in 1..num){
+        if (i % 2 == 0){
+            writer.write("\n $i")
+        }
+    }
+    writer.close()
+
 }
